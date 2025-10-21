d386d163e16ad3788e7726d1d874a49d558f4848
vagabond <athomec@pie.com.tw>
2025-10-21 17:23:04 +0800

    訂單功能調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,17 @@
 $(function () {//JS開頭
 
-	var WINDOW = $(window).width();//視窗寬度
-	var WINDOWH = $(window).height();//視窗高度
-
-	//---------------------訂單點擊出現選單設定------------------------
+	//---------------------訂單點擊出現選單
 	var mailList = $('.js-slidetoleft').each(function () {
 		var listItem = $(this);
 		var hammer = new Hammer(this);
 		var minX = -275; // 右側按鈕寬度
 		var maxX = 0;
 		var lastPosX;
-
 		hammer.on('tap', function (e) {
 			listItem.animate({ left: maxX + "px" }, 100);
 			listItem.addClass('active');
 			resetOtherItems(listItem);
 		});
-
 		function resetOtherItems(activeItem) {
 			$('.js-slidetoleft').not(activeItem).each(function () {
 				var item = $(this);
@@ -25,12 +20,81 @@ $(function () {//JS開頭
 			});
 		}
 	});
-	$('.js-slidetoleft').click(function () {//關閉其他補印選單
-		$(this).parent(".js-order-card-wrapper").siblings(".js-order-card-wrapper").find(".collapse").collapse("hide");
+	//---------------------訂單點擊時
+	$('.js-slidetoleft').click(function () {
+		$(this).parents("li").siblings("li").find(".js-order-dropdown-item").slideUp(200);
+		$(this).parents("li").siblings("li").find(".js-order-dropdown").removeClass("show");
 	})
-	$(".js-order-card-function").find("button:not(.order-card-function-menu button)").click(function () {
-		$(this).closest(".js-order-card-wrapper").find(".collapse").collapse("hide");
+	//---------------------訂單選單功能
+	$(".js-order-card-function button").click(function (e) {
+		const $btn = $(this);
+		const $li = $btn.closest("li");
+		const $otherLi = $li.siblings("li");
+		const $otherCards = $otherLi.find(".js-order-card");
+		const $otherCheck = $otherLi.find(".form-check-input:checked");//已選取的選取框
+		const $allDropdownItem = $(".js-order-dropdown-item");//所有次選單
+		const $dropdownBox = $li.find(".js-order-dropdown");//我的次選單框架
+		const $order = $(".js-order");//訂單容器
+
+		if ($btn.hasClass("js-dropdown-toggler")) { // 含次選單按鈕
+			const target = $btn.data("target"); 
+			const $dropdown = $li.find(".js-order-dropdown-item[data-con='" + target + "']");
+
+			// 先關掉同一個訂單下其他 dropdown
+			$li.find(".js-order-dropdown-item").not($dropdown).slideUp(200);
+			$dropdownBox.removeClass("show");
+			// 切換目前 dropdown
+			$dropdown.slideDown(200);
+			$dropdownBox.addClass("show");
+
+			if ($li.is(":last-child")) {//確保最後一個次選單露出
+				setTimeout(() => {
+					$order.animate({
+						scrollTop:
+							$dropdownBox.position().top + $order.scrollTop() - 200
+					}, 100);
+				}, 100);
+			}
+
+			if ($btn.hasClass("js-merge-btn")) {//若為合併按鈕
+				$otherCards.addClass("merging");
+			} else {
+				$otherCards.removeClass("merging");
+			}
+
+
+		} else { // 不含次選單按鈕
+			$allDropdownItem.slideUp(200);
+			$dropdownBox.removeClass("show");
+			$otherCards.removeClass("merging");
+			$otherCheck.addClass("god")
+			$otherCheck.trigger('click').trigger('change');
+		}
+		
 	});
+//擴大合併選取點擊範圍
+	$(".js-order-card").click(function (e) {
+		if($(this).hasClass("merging")){
+			$(this).find(".form-check-input").trigger('click').trigger('change');;
+		}
+	})
+
+	//---------------------訂單次選單功能
+	$(".js-order-dropdown a").click(function (e) {
+		if ($(this).is(".js-cancel-dropdown")){//如果是取消按鈕
+			const $dropdownItem = $(this).closest(".js-order-dropdown-item");//我的次選單
+			const $dropdownBox = $(this).closest(".js-order-dropdown");//我的次選單框架
+			const $otherCheck = $(this).closest(".js-order-dropdown").closest("li").siblings("li").find(".form-check-input:checked");//已選取的選取框
+			$dropdownItem.slideUp(200);
+			$dropdownBox.removeClass("show");
+			$(".js-order-card").removeClass("merging");
+			$otherCheck.trigger('click').trigger('change');
+		}
+	});
+
+
+
+
 	//---------------------切換按鈕設定------------------------
 	$('.js-select-list-btn').click(function () {
 		if ($(this).hasClass("active")) {
@@ -96,14 +160,6 @@ $(function () {//JS開頭
 		moveActiveBar($item);
 	});
 
-	//點餐視窗設定
-	/*const myOffcanvas = new bootstrap.Offcanvas('#order-btnlist', {
-		backdrop: false,   // 不顯示背景遮罩
-		scroll: true       // 讓背景可滾動與點擊
-	  });
-	$('.menu-card-item').click(function () {
-		offcanvas.show(); // 不會因重複點擊而關閉
-	});*/
 
 	//---------------------點餐左滑刪除設定------------------------
 	var mailList = $('.js-slidedelete').each(function () {
